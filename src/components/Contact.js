60abd0eeaed3a87b05024ddb189d780ee0408775
Vico4 <victoire.stahl@gmail.com>
2022-03-31 12:30:12 +0200

    formated contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,14 +29,14 @@ export const Contact = () => {
   return (
     <>
       <h1>Contact me</h1>
-      <form ref={form} onSubmit={sendEmail} action={Thanks}>
+      <form ref={form} onSubmit={sendEmail} action={Thanks} id="contact-form">
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="user_name" /> 
         <label>Email</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="user_email" /> 
         <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+        <textarea name="message" /> 
+        <input type="submit" value="Send" id="send"/>
       </form>
     </>
   );
